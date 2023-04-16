5ef819144b391554568abc1524adbe37d56dc75d
pushkarskayats <t.pushkarskaya@inbox.ru>
2023-04-16 19:44:50 +0300

    add day 18

---
+++ src/main/java/day18/Node.java
@@ -0,0 +1,32 @@
+package day18;
+
+public class Node {
+    int root;
+    Node nodeLeft;
+    Node nodeRight;
+
+    public Node(int root) {
+        this.root = root;
+    }
+
+    public Node(int root, Node nodeLeft, Node nodeRight) {
+        this.root = root;
+        this.nodeLeft = nodeLeft;
+        this.nodeRight = nodeRight;
+
+    }
+
+    public static void dfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.nodeLeft);
+        System.out.println(node.root);
+        dfs(node.nodeRight);
+    }
+
+}
+
+
+
+
--- src/main/java/day18/Task3.java
+++ src/main/java/day18/Task3.java
@@ -2,6 +2,20 @@ package day18;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        Node node = new Node(20,
+                new Node(14,
+                        new Node(11,
+                                new Node(5,
+                                        null, new Node(8)),
+                                null),
+                        new Node(16,
+                                new Node(15),
+                                new Node(18))),
+                new Node(23,
+                        new Node(22),
+                        new Node(27,
+                                new Node(24), new Node(150))));
+        node.dfs(node);
     }
+
 }
