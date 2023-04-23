bc8e7aa41d11fb3af7dad68ee38a954c96749b26
pushkarskayats <t.pushkarskaya@inbox.ru>
2023-04-23 14:24:35 +0300

    add day 18. Внесены изменения согласно замечаниям

--- src/main/java/day18/Node.java
+++ src/main/java/day18/Node.java
@@ -1,9 +1,9 @@
 package day18;
 
 public class Node {
-    int root;
-    Node nodeLeft;
-    Node nodeRight;
+    private int root;
+    private Node nodeLeft;
+    private Node nodeRight;
 
     public Node(int root) {
         this.root = root;
@@ -16,6 +16,28 @@ public class Node {
 
     }
 
+    public static void addNode(Integer number, Node node) {
+        if (node == null) {
+            throw new IllegalArgumentException("Корневой узел не может быть null");
+        }
+        Node nodeNext = node;
+        Node nodeCurrent = null;
+        while (nodeNext != null) {
+            nodeCurrent=nodeNext;
+            if (number < nodeCurrent.getRoot()) {
+                nodeNext = nodeCurrent.getNodeLeft();
+            } else {
+                nodeNext = nodeCurrent.getNodeRight();
+            }
+        }
+        if (number < nodeCurrent.getRoot()) {
+            nodeCurrent.setNodeLeft(new Node(number));
+        } else {
+            nodeCurrent.setNodeRight(new Node(number));
+        }
+
+    }
+
     public static void dfs(Node node) {
         if (node == null) {
             return;
@@ -23,6 +45,30 @@ public class Node {
         dfs(node.nodeLeft);
         System.out.println(node.root);
         dfs(node.nodeRight);
+    }
+
+    public Node getNodeLeft() {
+        return nodeLeft;
+    }
+
+    public void setNodeLeft(Node node) {
+        this.nodeLeft = node;
+    }
+
+    public Node getNodeRight() {
+        return nodeRight;
+    }
+
+    public void setNodeRight(Node node) {
+        this.nodeRight = node;
+    }
+
+    public int getRoot() {
+        return root;
+    }
+
+    public void setRoot(int root) {
+        this.root = root;
     }
 
 }
--- src/main/java/day18/Task3.java
+++ src/main/java/day18/Task3.java
@@ -2,20 +2,25 @@ package day18;
 
 public class Task3 {
     public static void main(String[] args) {
-        Node node = new Node(20,
-                new Node(14,
-                        new Node(11,
-                                new Node(5,
-                                        null, new Node(8)),
-                                null),
-                        new Node(16,
-                                new Node(15),
-                                new Node(18))),
-                new Node(23,
-                        new Node(22),
-                        new Node(27,
-                                new Node(24), new Node(150))));
-        node.dfs(node);
-    }
+//        Node node = new Node(20,
+//                new Node(14,
+//                        new Node(11,
+//                                new Node(5,
+//                                        null, new Node(8)),
+//                                null),
+//                        new Node(16,
+//                                new Node(15),
+//                                new Node(18))),
+//                new Node(23,
+//                        new Node(22),
+//                        new Node(27,
+//                                new Node(24), new Node(150))));
+//        node.dfs(node);
 
+        Node node = new Node(20);
+        int[] numbers = {14, 23, 11, 16, 22, 27, 5, 15, 18, 24, 150, 8};
+        for (int number : numbers)
+            Node.addNode(number, node);
+        Node.dfs(node);
+    }
 }
