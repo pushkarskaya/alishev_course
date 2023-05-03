27c36e89c59b39845cc73c6697a9c3e540cbd88f
pushkarskayats <t.pushkarskaya@inbox.ru>
2023-05-03 23:19:12 +0300

    add day 19 Task 1

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -18,14 +18,12 @@ public class Task1 {
                 String word = scanner.next();
                 if (wordToCount.containsKey(word))
                     wordToCount.put(word, wordToCount.get(word) + 1);
-
                 else {
                     wordToCount.put(word, 1);
                 }
             }
             for (String word : wordToCount.keySet()) {
                 System.out.println(word + " " + wordToCount.get(word));
-
             }
             if (wordToCount.containsKey("Чичиков")) {
                 System.out.println("Количество раз использования слова Чичиков:");
@@ -37,26 +35,13 @@ public class Task1 {
             Collections.sort(list, new Comparator() {
                 @Override
                 public int compare(Object o1, Object o2) {
-                    return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+                    return ((Comparable) ((Map.Entry) (o2)).getValue()).compareTo(((Map.Entry) (o1)).getValue());
                 }
             });
-            //Меняем порядок элементов в LinkedList
-            Collections.reverse(list);
-            HashMap newSortedMap = new LinkedHashMap();
-            for (Iterator i = list.iterator(); i.hasNext(); ) {
-                Map.Entry entry = (Map.Entry) i.next();
-                newSortedMap.put(entry.getKey(), entry.getValue());
+            System.out.println("100 самых часто используемых слов:");
+            for (Integer i = 0; i < 100; i++) {
+                System.out.println(list.get(i));
             }
-            System.out.println("Первые 100 самых часто употребляемых слов:");
-            Set set = newSortedMap.entrySet();
-            Iterator iterator = set.iterator();
-            int next = 0;
-            while (iterator.hasNext() && (next < 100)) {
-                Map.Entry map = (Map.Entry) iterator.next();
-                System.out.println(map.getKey() + " " + map.getValue());
-                next++;
-            }
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
